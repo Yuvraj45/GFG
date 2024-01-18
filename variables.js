bb634f3ab9427cd4fc4304c6e4f333bd017234d1
Yuvraj45 <y.gcs15039@gmail.com>
2024-01-18 21:35:53 +0530

    First js code

---
+++ variables.js
@@ -0,0 +1,14 @@
+const empID = 1173238
+let names = "yuvraj"
+var lastname  = "godase"
+
+abcd = "xyz"
+let names1;
+
+let co = 1173239
+
+console.log([empID, names , lastname, abcd, names1]);
+console.table([empID, names , lastname, abcd, names1, co]);
+
+
+
